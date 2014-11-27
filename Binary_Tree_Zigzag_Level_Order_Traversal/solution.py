c03d0aa0909d143c5c7cdc0594f6a7ffd11163f6
Little Leo <c2016.tw@gmail.com>
2014-11-27 14:28:59 -0500

    update Binary_Tree_Zigzag_Level_Order_Traversal

---
+++ Binary_Tree_Zigzag_Level_Order_Traversal/solution.py
@@ -0,0 +1,58 @@
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of lists of integers
+    def zigzagLevelOrder(self, root):
+        # do BFS
+        q = list()
+        q.append(root)
+        q.append(None)
+
+        ret = []
+        tmp_row = []
+        while len(q) > 0:
+            node = q.pop(0)
+            if(node == None):
+                if len(tmp_row) > 0:
+                    ret.append(tmp_row)
+                    tmp_row = []
+                    q.append(None)
+                    continue
+                else:
+                    break
+
+            tmp_row.append(node.val)
+            if not node.left == None:
+                q.append(node.left)
+            if not node.right == None:
+                q.append(node.right)
+
+        zigzag_ret = []
+        for i, l in enumerate(ret):
+            if i % 2 == 1:
+                zigzag_ret.append(l[::-1])
+            else:
+                zigzag_ret.append(l)
+
+        return zigzag_ret
+
+if __name__ == "__main__":
+    s = Solution()
+
+    t_n = TreeNode(3)
+    t_1 = TreeNode(9)
+    t_n.left = t_1
+    t_2 = TreeNode(20)
+    t_n.right = t_2
+    t_3 = TreeNode(15)
+    t_2.left = t_3
+    t_4 = TreeNode(7)
+    t_2.right = t_4
+
+    print s.zigzagLevelOrder(t_n)

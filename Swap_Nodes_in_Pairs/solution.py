fd450e6988fe2d24a6bd01696d3d28dc6ce8c3d7
Leo Hung <c2016.tw@gmail.com>
2014-12-18 04:53:58 +0800

    update Swap_Nodes_in_Pairs

---
+++ Swap_Nodes_in_Pairs/solution.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param a ListNode
+    # @return a ListNode
+    def swapPairs(self, head):
+        if head == None or head.next == None:
+            return head
+
+        tmp_head = ListNode(-1)
+        tmp_head.next = head
+
+        last = tmp_head
+
+        while last != None:
+            first = last.next
+            if first == None:
+                break
+            second = first.next
+            if second == None:
+                break
+
+            first.next, second.next = second.next, first
+            last.next = second
+
+        return tmp_head.next
+
+
